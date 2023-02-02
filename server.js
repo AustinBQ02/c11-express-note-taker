eeee7dd217374d76e9d43ccec5e243bd90c34efd
Michael Smith <mdsmith02@gmail.com>
2023-02-01 21:35:15 -0600

    all required tasks from #1 are done (not the bonus though)

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const notes = require("./db/db.json");
 const uuid = require("./helpers/uuid");
-
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,58 +15,89 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-
-
 // GET Route for ALL notes
-app.get("/api/notes", (req, res) => res.json(notes));
-
-
-// POST request to add a new note
-app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
-  
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        id: uuid(),
-      };
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
     } else {
-      res.status(500).json('Error in posting review');
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+    //   console.info('GET Request received for ALL notes: ');
+      res.status(201).json(parsedNotes);
     }
   });
+});
+
+// POST request to add a new note
+app.post("/api/notes", (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    // read the existing notes
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated reviews!")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting review");
+  }
+});
 
 // HTML Routes
 
 // GET Route for homepage
 app.get("/", (req, res) =>
-res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for notes page
 app.get("/notes", (req, res) =>
-res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // GET Route for wildcard
 app.get("*", (req, res) =>
-res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-
 
 // Start the server
 app.listen(PORT, () =>
